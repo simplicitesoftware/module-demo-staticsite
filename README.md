deeb4e67f3d571415cb7d422d7e438afbd735379
designer (Designer) <no-reply@simplicite.fr>
2022-11-01 19:00:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<!--
+ ___ _            _ _    _ _    __
+/ __(_)_ __  _ __| (_)__(_) |_ /_/
+\__ \ | '  \| '_ \ | / _| |  _/ -_)
+|___/_|_|_|_| .__/_|_\__|_|\__\___|
+            |_| 
+-->
+![](https://docs.simplicite.io//logos/logo250.png)
+* * *
+
+`DemoStaticSite` module definition
+==================================
+
+Custom static frontend for the order management demo
+
+`DemoStaticSite` external object definition
+-------------------------------------------
+
+Static frontend external object
+
+
---
+++ src/com/simplicite/extobjects/DemoStaticSite/DemoStaticSite.java
@@ -0,0 +1,31 @@
+package com.simplicite.extobjects.DemoStaticSite;
+
+import java.io.IOException;
+import com.simplicite.util.AppLog;
+import com.simplicite.util.tools.Parameters;
+
+/**
+ * Static frontend site
+ */
+public class DemoStaticSite extends com.simplicite.webapp.web.StaticSiteExternalObject {
+	private static final long serialVersionUID = 1L;
+
+	/* TODO: activate with 5.2.8
+	private static final String NOT_FOUND = "notfound.html";
+
+	@Override
+	public Object notfound(Parameters params)
+	{
+		AppLog.info("Not found on: " + params.getBaseLocation(), getGrant());
+		try {
+			if (NOT_FOUND.equals(params.getURI())) // Avoid infinite loop
+				throw new IOException();
+
+			return displayFile(NOT_FOUND);
+		} catch (IOException e) {
+			AppLog.error(null, e, getGrant());
+			return super.notfound(params);
+		}
+	}
+	*/
+}
