f4246f7a92866630450cdbe4328bd030bbb7176c
David AZOULAY <dazoulay@simplicite.fr>
2024-01-16 21:34:48 +0100

    Bump deps

--- others/.stylelintrc.js
+++ others/.stylelintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   'extends': 'stylelint-config-recommended-vue',
   'rules': {
-    'indentation': 2,
-    'declaration-empty-line-before': 'never',
-    'number-leading-zero': 'never'
+    'declaration-empty-line-before': 'never'
   }
 };
